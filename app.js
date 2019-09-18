20bcfb66a4d55554271d8c70aa44d425daaf7f01
genix <intergenixyt@gmail.com>
2019-09-18 17:53:11 +0200

    Add querying a single book and a single author.

--- app.js
+++ app.js
@@ -46,10 +46,30 @@ const RootQueryType = new GraphQLObjectType({
     name: 'Query',
     description: 'Root query.',
     fields: () => ({
+        book: {
+            type: BookType,
+            description: 'A single book.',
+            args: {
+                id: { type: GraphQLInt },
+            }, 
+            resolve: (parent, args) => {
+                return books.find(book => book.id === args.id)
+            }
+        },
         books: {
             type: new GraphQLList(BookType),
             description: 'List of all books.',
             resolve: () => books,
+        },
+        author: {
+            type: AuthorType,
+            description: 'A single author.',
+            args: {
+                id: { type: GraphQLInt }
+            },
+            resolve: (parent, args) => {
+                return authors.find(author => author.id === args.id)
+            } 
         },
         authors: {
             type: new GraphQLList(AuthorType),
